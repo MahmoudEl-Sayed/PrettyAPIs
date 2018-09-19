e1770d412a5cb66eb86377c0b5a7583c97fc2a03
Mahmoud EL-Sayed <m.elsayed@pitechnologies.net>
2018-09-19 16:33:44 +0200

    init

---
+++ PrettyAPIs/APIS_HANDLER.swift
@@ -0,0 +1,14 @@
+//
+//  APIS_HANDLER.swift
+//  PrettyAPIs
+//
+//  Created by Mahmoud EL-Sayed on 9/19/18.
+//  Copyright © 2018 Mahmoud EL-Sayed. All rights reserved.
+//
+
+import UIKit
+
+class APIS_HANDLER: NSObject {
+
+}
+
